296fe26ce4684d1e90cb606bb20f7605eabd0d07
dmwever <56411717+dmwever@users.noreply.github.com>
2025-11-22 16:43:14 -0600

    Create CampaignReader.py

---
+++ worlds/age2de/campaign/CampaignReader.py
@@ -0,0 +1,63 @@
+
+import os
+import struct
+import sys
+from typing_extensions import Buffer
+import numpy as np
+
+DE_DEPENDENCY_NUM = 7
+RGE_DE2_MAX_CHAR = 256
+
+RGE_STRING_ID = 0x0A60
+
+class CpnHeader:
+    version: int
+    name: str
+    scenarioNum: int
+    
+    def __init__(self, fp):
+        self.version = struct.unpack("i", fp.read(4))[0]
+        junk = struct.unpack("iiiiiii", fp.read(DE_DEPENDENCY_NUM*4))
+        self.name = struct.unpack(f"{str(RGE_DE2_MAX_CHAR)}s", fp.read(RGE_DE2_MAX_CHAR))[0]
+        self.scenarioNum = struct.unpack("i", fp.read(4))[0]
+    
+class Scenario:
+    size: int
+    offset: int
+    name: str
+    nameLen: int
+    fileName: str
+    fileNameLen: int
+    
+    def __init__(self, fp):
+        self.size = struct.unpack("i", fp.read(4))[0]
+        self.offset = struct.unpack("i", fp.read(4))[0]
+        de_str = struct.unpack("H", fp.read(2))[0]
+        self.nameLen = struct.unpack("H", fp.read(2))[0]
+        self.name = struct.unpack(f"{str(self.nameLen)}s", fp.read(self.nameLen))[0]
+        de_str = struct.unpack("H", fp.read(2))[0]
+        self.fileNameLen = struct.unpack("H", fp.read(2))[0]
+        self.fileName = struct.unpack(f"{str(self.fileNameLen)}s", fp.read(self.fileNameLen))[0]
+        
+        
+        
+
+class Campaign:
+    header: CpnHeader
+    scenarios: list[Scenario] = []
+    
+    def __init__(self, filename: str, outFolder: str):
+        with open(filename, "rb") as fp:
+            self.header = CpnHeader(fp)
+            
+            for x in range(self.header.scenarioNum):
+                scn = Scenario(fp)
+                self.scenarios.append(scn)
+            
+            os.makedirs(outFolder, exist_ok=True)
+            
+            for scn in self.scenarios:
+                with open(f"{outFolder}\\{scn.fileName.decode("utf-8")}", "wb") as out:
+                    fp.seek(scn.offset)
+                    scnFile = fp.read(scn.size)
+                    out.write(scnFile)
