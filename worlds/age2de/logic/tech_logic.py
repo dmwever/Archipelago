ec3640d4c8d2127fb9baba9fead533978bd265ec
dmwever <56411717+dmwever@users.noreply.github.com>
2026-09-24 23:00:36 -0500

    Update tech_logic.py

--- worlds/age2de/logic/tech_logic.py
+++ worlds/age2de/logic/tech_logic.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from rule_builder.rules import False_, Has, Rule, True_
+from rule_builder.rules import Has, Rule, True_
 
 from ..Options import LockTechs
-from ..locations.Ages import Age2AgeData
 from ..locations.Techs import Age2TechData
 
 
@@ -19,20 +18,7 @@ class TechLogic:
         self.logic = logic
         self.world = world
 
-    def can_research(self, tech: Age2TechData) -> Rule:
-        rule = self.has_tech_items(tech)
-        locked_at_start = self.world.tech_pool.locked_at_start(tech)
-        if not locked_at_start:
-            rule = rule & self.logic.ages.age_to_scenarios[tech.age]
-        elif tech.age > tech.buildings[0].age:
-            rule = rule & self.logic.can_reach_age(tech.age)
-        return rule
-    
-    def has_tech_items(self, tech: Age2TechData) -> Rule:
+    def has_tech_item(self, tech: Age2TechData) -> Rule:
         if self.world.options.lock_techs == LockTechs.option_effects:
             return True_()
-        rule: Rule = Has(tech.item.item_name)
-        prerequisite = tech.prerequisite
-        if prerequisite is not None and self.world.tech_pool.includes(prerequisite):
-            rule = rule & self.can_research(prerequisite)
-        return rule
+        return Has(tech.item.item_name)
